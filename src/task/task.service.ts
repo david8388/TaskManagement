00e1bf7a6e330ff4a4c5941392c0cde195e07a1a
David <hsienwu830808@gmail.com>
2020-06-08 22:11:24 +0800

    [Feat] Implement get all tasks

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    private tasks = []
+
+    getAllTasks() {
+        return this.tasks
+    }
+}
