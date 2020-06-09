1bde70fcebf10af615d627f29adb5bee8a0e71c7
David <hsienwu830808@gmail.com>
2020-06-09 21:23:57 +0800

    [Fix] Generate uuid function

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid';
+import { v1 as uuidv1 } from 'uuid';
 
 @Injectable()
 export class TaskService {
@@ -12,7 +12,7 @@ export class TaskService {
 
     createTask(title: string, description:string): Task {
         const task: Task = {
-            id: uuid(),
+            id: uuidv1(),
             title,
             description,
             status: TaskStatus.OPEN
