d772fdf2f050fcd5deb759f12fe33933e37cd37c
David <hsienwu830808@gmail.com>
2020-06-14 10:34:24 +0800

    [Fix] Entity not found errors

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: '172.17.0.3',
+  host: '127.0.0.1',
   port: 5432,
   username: 'postgres',
-  password: 'secretpassword',
+  password: 'mysecretpassword',
   database: 'taskmanagement',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  entities: ['dist/**/*.entity.js'], // https://stackoverflow.com/questions/59435293/typeorm-entity-in-nestjs-cannot-use-import-statement-outside-a-module
   synchronize: true,
 };
